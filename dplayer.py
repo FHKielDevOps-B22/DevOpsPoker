2033ff4bd702bf2598acf22fa21a6a3a77ab2a83
mahdi.ehsanifar <mahdi.ehsanifar@gmail.com>
2017-06-10 18:06:10 +0200

    Final

--- dplayer.py
+++ dplayer.py
@@ -9,13 +9,14 @@
 # Configuration
 # You need to change the setting according to your environment
 gregister_url='http://192.168.0.5:5001'
-glocalip_adr='127.0.0.1'
+glocalip_adr='192.168.0.35'
 
 # -----------------------------------------------------------
 
 from flask import Flask, request
 from flask_restful import Resource, Api
 import sys
+import random
 
 from requests import put
 import json
@@ -63,17 +64,69 @@ class PokerPlayerAPI(Resource):
     #                                     'd' : diamonds
     #                                     'c' : clubs
     #
-    # @return a dictionary containing the following values
+    # @retudatarn a dictionary containing the following values
     #         bid  : a number between 0 and max_bid
     def __get_bid(self, data):
+        result = 0
+        bigBlind = int(data['big_blind'])
+        minBid = int(data['min_bid'])
+        maxBid = int(data['max_bid'])
+        handList = list(data['hand'])
 
-        return 0
+        boardList = list(data['board'])
+        firstHandCard = str(handList[0])
+        secondHandCard = str(handList[1])
+        #firstBoardCard = str(boardList[0])
+        #secondBoardCard = str(boardList[1])
+        #thirdBoardCard = str(boardList[2])
+        #forthBoardCard = str(boardList[3])
+        #fifthBoardCard = str(boardList[4])
+
+        print('dataL ', data)
+
+        if firstHandCard[0] == secondHandCard[0]:
+            if ('A', 'K', 'Q', 'J', 'T').__contains__(firstHandCard[0]):
+                print("Perfect!")
+                result = maxBid
+            else:
+                raiseValue = (maxBid - minBid)
+                print(raiseValue)
+                if raiseValue > 100:
+                    result = minBid + 20
+                else:
+                    randomValue = random.randint(0, 1)
+                    if randomValue == 1:
+                        result = maxBid
+                    else:
+                        result = minBid
+                        print('random: ', randomValue)
+        else:
+            if firstHandCard[1] == secondHandCard[1]:
+                randomValue = random.randint(0, 1)
+                if randomValue == 1:
+                    result = minBid
+                    print('random: ', randomValue)
+            else:
+                if (firstHandCard[0] == 'A' and secondHandCard[0] == 'K' or firstHandCard[0] == 'K' and secondHandCard[0] == 'A' ) and firstHandCard[1] == secondHandCard[1]:
+                    result = maxBid
+                else:
+                    if ('A', 'K', 'Q', 'J', 'T').__contains__(firstHandCard[0]) and ('A', 'K', 'Q', 'J', 'T').__contains__(secondHandCard[0]):
+                        result = maxBid
+                    else:
+                        if minBid == bigBlind and random.randint(0, 1)==1:
+                            result = minBid + 20
+
+
+        print('result: ',result)
+        return result
 
     # dispatch incoming get commands
     def get(self, command_id):
 
         data = request.form['data']
         data = json.loads(data)
+        #print(self.__get_bid(self.__get_bid(data)))
+
 
         if command_id == 'get_bid':
             return {'bid': self.__get_bid(data)}
@@ -82,6 +135,8 @@ class PokerPlayerAPI(Resource):
 
     # dispatch incoming put commands (if any)
     def put(self, command_id):
+        print("put")
+
         return 201
 
 
